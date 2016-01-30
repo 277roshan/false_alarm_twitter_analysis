678498dd7c8c5a1faf0a30170e1bb5c5d04a3b6d
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2016-01-30 03:40:40 -0500

    index file added

--- basic-server.js
+++ basic-server.js
@@ -10,6 +10,7 @@ var app = express();
 //Let's set up a port for our server to listen on
 var port = 8300;
 
+var path = require('path');
 //Just one basic server response, to make sure our server is working.
 // app.get('/*', function(req, res){  
 //   res.send('Hello World');
@@ -40,8 +41,9 @@ client.get('friends/ids', params, function(error, tweets, response){
   if (!error) {
     console.log(tweets);
 
-    app.get('/*',function(req,res){
-        res.send(tweets);
+    app.get('/',function(req,res){
+      res.sendFile(path.join(__dirname + '/index.html'));
+      //res.send(tweets);
     });
   }
 });
